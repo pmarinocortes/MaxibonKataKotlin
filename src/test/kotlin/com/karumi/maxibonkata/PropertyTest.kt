d1567ebc8a5d1a4012e32504404e1a2ba77bf0f8
Pablo Mariño Cortes <p.marino.cortes@mobgen.com>
2019-11-14 10:29:19 +0100

    test

--- src/test/kotlin/com/karumi/maxibonkata/PropertyTest.kt
+++ src/test/kotlin/com/karumi/maxibonkata/PropertyTest.kt
@@ -6,6 +6,7 @@ import io.kotlintest.specs.ShouldSpec
 
 
 class PropertyTest : ShouldSpec() {
+    //tests
     init {
         should("return the length of the string").config(invocations = 10, threads = 2) {
             "sammy".length shouldBe 5
